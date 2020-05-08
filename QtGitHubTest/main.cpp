092e00a41b7a16600f689d15cb9c6c73583fe65e
HaijieZhu <1286259934@qq.com>
2020-05-08 14:49:49 +0800

    添加了std::cout

--- QtGitHubTest/main.cpp
+++ QtGitHubTest/main.cpp
@@ -1,10 +1,12 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
+	std::cout << "hello world" << std::endl;
 	return a.exec();
 }
